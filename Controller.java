1eb1b5c5c33b1b9862df2a8906e8bfb075489773
Samuel Ellertson <samuelEllertson@hotmail.com>
2019-12-04 08:35:25 -0800

    added empty moethds to controller

--- Controller.java
+++ Controller.java
@@ -24,4 +24,53 @@ public class Controller{
         */
     }
 
+    //implements issue #5
+    //returns true on success, false on failure
+    public bool CreateReservation(){
+        return false;
+    }
+
+    //implements issue #6
+    //returns true on success, false on failure
+    public bool CancelReservation(){
+        return false;
+    }
+
+    //implements issue #7
+    //returns true on success, false on failure
+    public bool RoomInformation(){
+        return false;
+    }
+
+    //implements issue #10
+    //returns true on success, false on failure
+    public bool RoomQuery(){
+        return false;
+    }
+
+    //implements issue #11
+    //returns true on success, false on failure
+    public bool RoomAvailability(){
+        return false;
+    }
+
+    //implements issue #12
+    //returns true on success, false on failure
+    public bool UpdateReservation(){
+        return false;
+    }
+
+    //implements issue #13
+    //returns true on success, false on failure
+    public bool DisplayReservations(){
+        return false;
+    }
+
+    //implements issue #14
+    //returns true on success, false on failure
+    public bool RevenueOverview(){
+        return false;
+    }
+
+
 }
