5ad8a2deba4b4e2aafffffa7618348f5701ff64b
Samuel Ellertson <samuelEllertson@hotmail.com>
2019-12-04 07:17:26 -0800

    removed extraneous DAO stuff

--- DaoCommand.java
+++
@@ -1,5 +0,0 @@
-public interface DaoCommand {
-  Object execute(DaoManager daoManager);
-} //what is the point of this? Probably needed when we actually 
-// create transactions using prepared statements, but if that's the case
-// why not just make it preparedstatemnts tho?
