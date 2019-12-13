c89089214fd38a24d4d3e6eb9e92d7af659392d7
Brandon Lyday <bwlyday@gmail.com>
2019-12-12 23:56:24 -0800

    nearly done with ManagerView

--- src/ManagerView.java
+++ src/ManagerView.java
@@ -1,10 +1,27 @@
+import java.util.Map;
+
 public class ManagerView extends View {
 
     public ManagerView(Controller controller){
         super(controller);
     }
 
-    public void PrintMenuItems(){}
+    public void PrintMenuItems() {
+        String[] options = {
+                "1) view monthly revenue",
+                "2) log out"
+        };
+        for(String option: options){
+            print(option);
+        }
+    }
 
-    public void doUILoop(User user){}
+    public void revenueDisplay(){
+        Map<String, Map<String, Integer>> roomMonthlyRevenue = controller.roomsReservedDAO.getRoomsMonthlyRevenue();
+        System.out.println(roomMonthlyRevenue.toString());
+    }
+
+    public void doUILoop(User user){
+        revenueDisplay();
+    }
 }
