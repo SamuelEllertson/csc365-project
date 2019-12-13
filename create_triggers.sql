4cb322895b61af232f07f3f42b8ccc35952d05ca
Brandon Lyday <bwlyday@gmail.com>
2019-12-13 12:41:53 -0800

    added triggers to charge and refund CreditCard

--- create_triggers.sql
+++ create_triggers.sql
@@ -1,4 +1,34 @@
-/*
-check if they can pay
-check if rooms are available 
-*/
+DELIMITER $
+CREATE TRIGGER charge_credit_card
+    AFTER INSERT ON RoomsReserved
+    FOR EACH ROW
+    BEGIN
+        SET @cost = (SELECT DATEDIFF(CheckOut, CheckIn)*Price AS cost FROM RoomsReserved rr
+                        JOIN Reservation r ON rr.ReservationId = r.ReservationId
+                        JOIN Room ro ON rr.RoomId = ro.RoomId
+                        WHERE rr.ReservationId = NEW.ReservationId AND rr.RoomId = NEW.RoomId);
+        SET @card = (SELECT CardId FROM Reservation r WHERE r.ReservationId = NEW.ReservationId);
+
+        IF((SELECT balance FROM CreditCard WHERE CardId = @card) + @cost > (SELECT climit FROM CreditCard WHERE CardId = @card)) THEN
+            SIGNAL SQLSTATE '12345'
+            SET MESSAGE_TEXT = 'Check constraint failed: CreditCards.balance would be greater than CreditCards.climit';
+        END IF;
+
+        UPDATE CreditCard SET balance = balance + @cost WHERE CardId = @card;
+    END$
+DELIMITER ;
+
+DELIMITER $
+CREATE TRIGGER refund_credit_card
+    BEFORE DELETE ON RoomsReserved
+    FOR EACH ROW
+    BEGIN
+        SET @cost = (SELECT DATEDIFF(CheckOut, CheckIn)*Price AS cost FROM RoomsReserved rr
+                        JOIN Reservation r ON rr.ReservationId = r.ReservationId
+                        JOIN Room ro ON rr.RoomId = ro.RoomId
+                        WHERE rr.ReservationId = OLD.ReservationId AND rr.RoomId = OLD.RoomId);
+        SET @card = (SELECT CardId FROM Reservation r WHERE r.ReservationId = OLD.ReservationId);
+
+        UPDATE CreditCard SET balance = balance - @cost WHERE CardId = @card;
+    END$
+DELIMITER ;
